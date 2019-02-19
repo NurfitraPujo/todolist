1fa73dc7db9901ec3d1b29e152d47b21db78c564
NurfitraPujo <nurfitrapujo.19@gmail.com>
2019-02-19 22:46:03 +0700

    fix some css, and checkbox to the task

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
-function todoList() {
+function addTask() {
+    //take value from text input
     let item = document.getElementById("todoInput").value;
-    let text = document.createTextNode(item);
-    let newItem = document.createElement("li");
-    newItem.appendChild(text);
-    document.getElementById("todoList").appendChild(newItem);
+    if (item == "") {
+        //return when the input is empty
+        return;
+    }
+    else {
+        let text = document.createTextNode(item);
+        let newItem = document.createElement("li");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = "checkTask";
+        checkbox.onclick = function () {
+            finish(newItem);
+        }
+
+        checkbox.classList.add("check");
+        newItem.classList.add("task");
+
+        newItem.appendChild(checkbox);
+        newItem.appendChild(text);
+        document.getElementById("todoList").appendChild(newItem);
+        document.getElementById("todoInput").value = "";
+    }
+}
+
+//change checkbox color
+// function isChecked(elem) {
+//     elem.parentNode.style.color = (elem.checked) ? '#fff' : '#fff';
+// }
+
+//move finished task 
+function finish(newItem) {
+    newItem.id = "finished";
+    document.getElementById("finishedTitle").style.visibility = "visible";
+
+    let moved = document.getElementById("finished");
+    let parent = moved.parentElement;
+    let reference = parent.querySelector("#finishedList");
+
+    parent.appendChild(moved, reference);
 }
